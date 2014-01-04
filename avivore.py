bd8a23c2c1c70dc7e796504b4dcdf0de01a32a1f
rc0r <hlt99@blinkenshell.org>
2014-01-04 20:59:38 +0100

    Heavy refactoring & code clean up performed (fixes #2)

--- avivore.py
+++ avivore.py
@@ -5,234 +5,250 @@ import time
 from twitter import *
 # from oauth import *
 import re
+# import urllib
 import sys
 import os
-import locale
 import sqlite3 as lite
 import ConfigParser
-
-"""
-Here are the various settings we'll want to use for the application.
-"""
-TwitterInstance = None
-TwitterSearchTerms = []
-TwitterTrackKeywords = None
-TwitterSearchTypes = []
-TwitterSearchInterval = 30 # You'll want to set this to ten seconds or higher.
-BearerToken = None
-CredsFile = None
-ConsumerKey = None
-ConsumerSecret = None
-DBPath = None
 
 """
 Helper functions
 """
 # check if arg is list, tuple, ...
 def is_sequence(arg):
-	return (not hasattr(arg, "strip") and
-		hasattr(arg, "__getitem__") or
-		hasattr(arg, "__iter__"))
+    return (not hasattr(arg, "strip") and
+            hasattr(arg, "__getitem__") or
+            hasattr(arg, "__iter__"))
 
-"""
-Twitter-related functions.
-"""
-def TwitterAuth(credsFile):
-	MY_TWITTER_CREDS = os.path.expanduser(credsFile)
-	if not os.path.exists(MY_TWITTER_CREDS):
-		oauth_dance("twatting_search_cli", ConsumerKey, ConsumerSecret, MY_TWITTER_CREDS)
 
-	oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
-	twitter = TwitterStream(auth=OAuth(oauth_token, oauth_secret, ConsumerKey, ConsumerSecret), )
-	return twitter
+class AvivoreConfig:
+    def __init__(self, config_filename):
+        self.config_filename = str(config_filename)
+        self.config = ConfigParser.ConfigParser()
+        self.twitter_search_types = []
+        self.twitter_search_terms = []
+        self.twitter_track_keywords = None
+        self.twitter_consumer_key = None
+        self.twitter_consumer_secret = None
+        self.twitter_search_interval = 30
+        self.credentials_file = None
+        self.database_path = None
 
-def TwitterSearch(string):
-    try:
-	TwitterRetr = TwitterInstance.search.tweets(q=string)
-	output = TwitterRetr['statuses']
-    except:
-	output = None # If this bombs out, we have the option of at least spitting out a result.
-    return output
+    def read_config(self):
+        # open config file
+        self.config.read(self.config_filename)
 
-def TwitterReadTweet(string):
-	i=-1
-	for x in TwitterSearchTypes:
-		i=i+1
-		FindType = re.compile(x)
-		result_raw = FindType.findall(string)
-		if result_raw == []:
-			continue
-		else:
-			if(is_sequence(result_raw[0])):
-				result = [x for x in result_raw[0] if x]
-			else:
-				result = result_raw[0]
+        # read data set definitions
+        exists = True
+        i = 0
+        while exists:
+            try:
+                self.twitter_search_types.append(self.config.get('twitter_search_objects', str(i), 0).strip("'\""))
+                i += 1
+            except:
+                exists = False
 
-			if(is_sequence(result)):
-				result = result[0]
-				
-			return i, result
-	# nothing found
-	return -1, 0
+        # read search term definitions
+        twitter_search_term = self.config.get('twitter_search', 'csv_search_term', 0).strip("'\"")
+        twitter_search_terms_raw = twitter_search_term.split(',')
+        for x in twitter_search_terms_raw:
+            self.twitter_search_terms.append(x.strip(" '\n"))
+
+        # read other settings
+        self.twitter_consumer_key = self.config.get('twitter_auth', 'consumer_key', 0).strip("'\"")
+        self.twitter_consumer_secret = self.config.get('twitter_auth', 'consumer_secret', 0).strip("'\"")
+        self.database_path = self.config.get('database', 'dbpath', 0).strip("'\"")
+        self.credentials_file = self.config.get('twitter_auth', 'credentials_file', 0).strip("'\"")
+        self.twitter_search_interval = int(self.config.get('twitter_search', 'interval', 0).strip("'\""))
+        self.twitter_track_keywords = self.config.get('twitter_search', 'stream_tracking_keyword', 0).strip(" '\"")
+
+#       self.twitter_track_keywords = urllib.quote(self.config.get('twitter_search',
+#                                           'stream_tracking_keyword', 0)).strip("'\"").replace('%2C', ',')
+
+    def init_database(self, status):
+        if os.path.isfile(self.database_path):
+            Output("Using existing database to store results.")
+            dbcon = lite.connect(self.database_path)
+            dbcur = dbcon.cursor()
+            dbcur.execute("SELECT Count(*) FROM Data")
+            Output(str(dbcur.fetchone()[0]) + " entries in this database so far.")
+            '''
+            # Removed the items below due to a bug. It's not needed really.
+            DBCur.execute("SELECT * FROM Data ORDER BY TimeRecv ASC LIMIT 1")
+            print DBCur.fetchone()[0]
+            DatabaseFirstWrite = float(DBCur.fetchone()[0]) + 2
+            Output("Database first written to " + str(DataBaseFirstWrite))
+            '''
+        else:   # If the database doesn't exist, we'll create it.
+            if status == 1:     # If we desire to save the database, it will output this message.
+                Output("Creating a new database to store data!")
+                # Eventually I'll set this up to just delete the DB at close should it be chosen as an option.
+            dbcon = lite.connect(self.database_path)
+            dbcur = dbcon.cursor()
+            dbcur.execute(
+                "CREATE TABLE Data (TimeRecv int, Type int, User text, UserId text, Value text, TID int, Message text)")
+
+
+class Avivore:
+    def __init__(self, avivore_config):
+        assert isinstance(avivore_config, AvivoreConfig)
+        self.avivore_config = avivore_config
+        self.twitter_stream_instance = None
+        self.twitter_instance = None
+        self.twitter_bearer_token = None
+
+    """
+    Twitter-related functions.
+    """
+
+    def twitter_stream_auth(self):
+        my_twitter_creds = os.path.expanduser(self.avivore_config.credentials_file)
+        if not os.path.exists(my_twitter_creds):
+            oauth_dance("twatting_search_cli", self.avivore_config.twitter_consumer_key,
+                        self.avivore_config.twitter_consumer_secret, my_twitter_creds)
+
+        oauth_token, oauth_secret = read_token_file(my_twitter_creds)
+        self.twitter_stream_instance = TwitterStream(auth=OAuth(oauth_token, oauth_secret,
+                                                                self.avivore_config.twitter_consumer_key,
+                                                                self.avivore_config.twitter_consumer_secret))
+        return self.twitter_stream_instance
+
+    """
+    classic Twitter search query
+    """
+
+    def twitter_search(self, search_string):
+        try:
+            twitter_retr = self.twitter_instance.search.tweets(q=search_string)
+            output = twitter_retr['statuses']
+        except:
+            output = None   # If this bombs out, we have the option of at least spitting out a result.
+        return output
+
+    def twitter_read_tweet(self, string):
+        i = -1
+        for x in self.avivore_config.twitter_search_types:
+            i += 1
+            findtype = re.compile(x)
+            result_raw = findtype.findall(string)
+            if not result_raw:
+                continue
+            else:
+                if is_sequence(result_raw[0]):
+                    result = [x for x in result_raw[0] if x]
+                else:
+                    result = result_raw[0]
+
+                if is_sequence(result):
+                    result = result[0]
+
+                return i, result
+                # nothing found
+        return -1, 0
+
+    def process_tweet(self, string):
+        # This is just to write the tweet to the DB and then to output it in a
+        # friendly manner. I guess it can be cleaned up but it works.
+        if self.__db_dup_check(string[3]) == 0:
+            self.__db_write_value(time.time(), string[0], string[1], string[5],
+                                  string[2], string[3], string[4])
+            return "Type: " + str(string[0]) + ", User: " + string[1] + " (" + \
+                   string[5] + "), Content: " + string[2] + ", TID: " + str(string[3])
+        else:
+            return 0
+
+    def __db_dup_check(self, value):
+        # The nice thing about using the SQL DB is that I can just have it make a query to make a duplicate check.
+        # This can likely be done better but it's "good enough" for now.
+        string = "SELECT * FROM Data WHERE TID IS \'" + str(int(value)) + "\'"
+        con = lite.connect(self.avivore_config.database_path)
+        cur = con.cursor()
+        cur.execute(string)
+        if cur.fetchone() is not None:  # We should only have to pull one.
+            output = 1
+        else:
+            output = 0
+        return output
+
+    def __db_write_value(self, stime, stype, user, userid, value, tweetid, message):
+        # Just a simple function to write the results to the database.
+        con = lite.connect(self.avivore_config.database_path)
+        qstring = "INSERT INTO Data VALUES(?, ?, ?, ?, ?, ?, ?)"
+        with con:
+            cur = con.cursor()
+            cur.execute(qstring,
+                        (unicode(stime), unicode(stype), unicode(user), unicode(userid),
+                         unicode(value), unicode(tweetid), unicode(message)))
+            #lid = cur.lastrowid
+
 
 """
 Various functions for this application.
 """
-def Main():
-    LastAction = time.time() # Sets the initial time to start our scan. It's not used however.
-    Stored = []
-#     global BearerToken
-#     BearerToken = TwitterAuth()
-    global TwitterInstance
-    TwitterInstance = TwitterAuth(CredsFile)
-    iterator = TwitterInstance.statuses.filter(track=TwitterTrackKeywords)
-#     while 1:
-#        for x in TwitterSearchTerms:
-#            TwitData = TwitterSearch(x)
+
+
+def main(argv):
+    config = AvivoreConfig(argv[1])
+    config.read_config()
+    config.init_database(0)
+
+    stored = []
+    avivore = Avivore(config)
+    twitter_stream_inst = avivore.twitter_stream_auth()
+    print config.twitter_track_keywords
+
+    iterator = twitter_stream_inst.statuses.filter(track=config.twitter_track_keywords)
+
     for y in iterator:
-#             if TwitData is None: # With the defaults, it's unlikely that this message will come up.
-#                 message = "Nothing found for \"" + x + "\". Waiting " + str(TwitterSearchInterval) + " seconds to try again."
-#                 Output(message)
-#             else:
-#                 for y in TwitterSearch(x):
-	    if 'id' in y and 'created_at' in y and 'user' in y and 'text' in y:
-# 	        print y
-                z = y['id'], y['created_at'], y['user']['screen_name'], y['text'], y['user']['id_str']
-                result = TwitterReadTweet(z[3])
-                if result[0] < 0:
-                    pass
-                else: # If something is found, then we'll process the tweet
-                    Stored = Stored, int(z[0])
-                    string = result[0], z[2], str(result[1]), z[0], z[3], z[4] # result value, time, result itself, tweet ID, tweet itself, userId
-                    message = ProcessTweet(string)
-                    Output(message)
-#             time.sleep(TwitterSearchInterval) # This will pause the script 
+        if 'id' in y and 'created_at' in y and 'user' in y and 'text' in y:
+            # print y
+            z = y['id'], y['created_at'], y['user']['screen_name'], y['text'], y['user']['id_str']
+            result = avivore.twitter_read_tweet(z[3])
+            if result[0] < 0:
+                pass
+            else:   # If something is found, then we'll process the tweet
+                stored = stored, int(z[0])
+                # result value, time, result itself, tweet ID, tweet itself, userId
+                string = result[0], z[2], str(result[1]), z[0], z[3], z[4]
+                message = avivore.process_tweet(string)
+                Output(message)
 
-def ReadConfig(string):
-	# Very quick and really dirty! >:-}
-	# Feel free to do some clean up...
-	global DBPath
-	global CredsFile
-	global TwitterSearchTypes
-	global TwitterSearchInterval
-	global TwitterTrackKeywords
-	global TwitterSearchTerms
-	global ConsumerKey
-	global ConsumerSecret
-
-	config = ConfigParser.ConfigParser()
-	# open config file
-	config.read(string)
-	
-	# read data set definitions
-	exists=True
-	i=0
-	while(exists==True):
-		try:
-			TwitterSearchTypes.append( config.get('twitter_search_objects', str(i), 0).strip("'") )
-			i = i+1
-		except:
-			exists=False
-	
-	# read search term definitions
-	twitter_search_term = config.get('twitter_search', 'csv_search_term', 0)
-	twitter_search_terms_raw = twitter_search_term.split( ',' )
-	for x in twitter_search_terms_raw:
-		TwitterSearchTerms.append( x.strip(" '\n") )
-
-	# read other settings
-	ConsumerKey = config.get('twitter_auth', 'consumer_key', 0)
-	ConsumerSecret = config.get('twitter_auth', 'consumer_secret', 0)
-	DBPath = config.get('database', 'dbpath', 0)
-	CredsFile = config.get('twitter_auth', 'credentials_file', 0)
-	TwitterSearchInterval = int(config.get('twitter_search', 'interval', 0))
-	TwitterTrackKeywords = config.get('twitter_search', 'stream_tracking_keyword', 0)
 
 def Output(string):
     # Default text output for the console.
-    if string == 0:
+    if 0 == string:
         pass
     else:
-        print "[" + str(round(time.time(),0))[:-2] + "]", string # This is sort of lame but whatever.
+        # This is sort of lame but whatever:
+        print "[" + str(round(time.time(), 0))[:-2] + "]", string
 
-def ProcessTweet(string): 
-    # This is just to write the tweet to the DB and then to output it in a friendly manner.
-    # I guess it can be cleaned up but it works.
-    if DBDupCheck(string[3]) == 0:
-        DBWriteValue(time.time(), string[0], string[1], string[5], string[2], string[3], string[4])
-	return "Type: " + str(string[0]) + ", User: " + string[1] + " (" + string[5] + "), Content: " + string[2] + \
-               ", TID: " + str(string[3])
-    else:
-        return 0
-
-def DBDupCheck(value):
-    # The nice thing about using the SQL DB is that I can just have it make a query to make a duplicate check.
-    # This can likely be done better but it's "good enough" for now.
-    string = "SELECT * FROM Data WHERE TID IS \'" + str(int(value)) + "\'"
-    con = lite.connect(DBPath)
-    cur = con.cursor()
-    cur.execute(string)
-    if cur.fetchone() != None: # We should only have to pull one.
-        output = 1
-    else:
-        output = 0
-    return output
-
-def DBWriteValue(Time, Type, User, UserId, Value, TweetID, Message):
-    # Just a simple function to write the results to the database.
-    con = lite.connect(DBPath)
-    qstring = "INSERT INTO Data VALUES(?, ?, ?, ?, ?, ?, ?)"
-    with con:
-        cur = con.cursor()
-        cur.execute(qstring, ( unicode(Time), unicode(Type), unicode(User), unicode(UserId), unicode(Value), unicode(TweetID), unicode(Message) ) )
-        lid = cur.lastrowid
-
-def InitDatabase(status, filename):
-    if os.path.isfile(filename):
-        Output("Using existing database to store results.")
-        DBCon = lite.connect(filename)
-        DBCur = DBCon.cursor()
-        DBCur.execute("SELECT Count(*) FROM Data")
-        Output(str(DBCur.fetchone()[0]) + " entries in this database so far.")
-	'''
-        # Removed the items below due to a bug. It's not needed really.
-        DBCur.execute("SELECT * FROM Data ORDER BY TimeRecv ASC LIMIT 1")
-        print DBCur.fetchone()[0]
-        DatabaseFirstWrite = float(DBCur.fetchone()[0]) + 2
-        Output("Database first written to " + str(DataBaseFirstWrite))
-        '''
-    else: # If the database doesn't exist, we'll create it.
-        if status == 1: # If we desire to save the database, it will output this message.
-            Output("Creating a new database to store data!")
-        # Eventually I'll set this up to just delete the DB at close should it be chosen as an option.
-        DBCon = lite.connect(filename)
-        DBCur = DBCon.cursor()
-        DBCur.execute("CREATE TABLE Data (TimeRecv int, Type int, User text, UserId text, Value text, TID int, Message text)")
 
 def SoftwareInitMsg(version):
     print "AvivoreXT", version, "by rc0r (https://github.com/rc0r)"
 
-def CheckUsage(argv):
-	if(len(argv)!=2):
-		print "Usage: %s <config-file>" % argv[0]
-		sys.exit(-1)
 
-def SoftwareExit(type, message):
+def CheckUsage(argv):
+    if 2 != len(argv):
+        print "Usage: %s <config-file>" % argv[0]
+        sys.exit(-1)
+
+
+def SoftwareExit(status, message):
     print ""
     print message
+    sys.exit(status)
+
 
 """
 Here we go!
 """
 if __name__ == "__main__":
-    SoftwareInitMsg("1.1.1")
+    SoftwareInitMsg("1.2")
     CheckUsage(sys.argv)
-    ReadConfig(sys.argv[1])
-    InitDatabase(0, DBPath)
     try:
-        Main()
+        main(sys.argv)
     except KeyboardInterrupt:
         SoftwareExit(0, "Exiting the application.")
     except:
-        Main()
+        main(sys.argv)
         raise
