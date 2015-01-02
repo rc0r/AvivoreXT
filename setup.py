81b6d888d640c3599a5f2ab1ea03d86459ac9452
rc0r <hlt99@blinkenshell.org>
2015-01-02 13:09:15 +0100

    setup.py updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import AvivoreXT
 from setuptools import setup, find_packages
 
 dependencies = ['twitter']
-
+used_license = 'GNU Library or Lesser General Public License (LGPL)'
 scripts = ['avivore']
 
 setup(name='AvivoreXT',
@@ -13,10 +13,18 @@ setup(name='AvivoreXT',
       long_description=open("./README.md", "r").read(),
       classifiers=[
           'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: ' + used_license,
+          'Operating System :: OS Independent',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
+          'Topic :: Utilities',
       ],
-      author='@_rc0r',
+      platforms=[
+          'Any',
+      ],
+      license=used_license,
+      author='rc0r',
+      author_email='hlt99@blinkenshell.org',
       url='https://github.com/rc0r/AvivoreXT',
       install_requires = dependencies,
       scripts=scripts,
